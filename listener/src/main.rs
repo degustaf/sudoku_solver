e425e3b48c7fe64c69fda26f468862b2bbd073d8
Derek Gustafson <degustaf@gmail.com>
2023-04-01 20:30:21 -0400

    Change websockets from warp to underlying tungstenite. This should speed up build times.

--- listener/src/main.rs
+++ listener/src/main.rs
@@ -6,21 +6,21 @@
 #![warn(missing_docs)]
 use crate::types::Response;
 use futures_util::{SinkExt, StreamExt, TryFutureExt};
+use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::mpsc;
 use tokio_stream::wrappers::ReceiverStream;
 use tokio_util::sync::CancellationToken;
-use warp::filters::ws::Message;
-use warp::ws::{WebSocket, Ws};
-use warp::{cors, Filter};
+use tungstenite::protocol::Message;
 
 mod requests;
 mod types;
 
-/// This function is heavily based on the
-/// [warp example](https://github.com/seanmonstar/warp/blob/master/examples/websockets_chat.rs).
-async fn handler(ws: WebSocket) {
+async fn handler(ws: TcpStream) {
     println!("Connection established.");
-    let (mut ws_tx, mut ws_rcv) = ws.split();
+    let ws_stream = tokio_tungstenite::accept_async(ws)
+        .await
+        .expect("Error during the websocket handshake occurred");
+    let (mut ws_tx, mut ws_rcv) = ws_stream.split();
 
     let (ch_tx, ch_rx) = mpsc::channel::<Response>(100); // I would be very surprised if we get anywhere close to this.
     let mut ch_rx = ReceiverStream::new(ch_rx);
@@ -42,14 +42,16 @@ async fn handler(ws: WebSocket) {
         let token = CancellationToken::new();
         let msg = match result {
             Ok(msg) => {
-                if msg.is_close() {
-                    eprintln!("Connection closed.");
-                    break;
-                } else if let Ok(msg) = msg.to_str() {
-                    String::from(msg)
-                } else {
-                    eprintln!("Unable to process binary message from websocket: {msg:?}");
-                    continue;
+                match msg {
+                    Message::Text(msg) => msg,
+                    Message::Close(_) => {
+                        eprintln!("Connection closed.");
+                        break;
+                    }
+                    _ => {
+                        eprintln!("Unable to process binary message from websocket: {msg:?}");
+                        continue;
+                    }
                 }
             }
             Err(e) => {
@@ -64,9 +66,10 @@ async fn handler(ws: WebSocket) {
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    let route = warp::path::end()
-        .and(warp::ws())
-        .map(|ws: Ws| ws.on_upgrade(handler))
-        .with(cors().allow_any_origin());
-    warp::serve(route).run(([127, 0, 0, 1], 4545)).await;
+    let addr = "127.0.0.1:4545";
+    let listener = TcpListener::bind(&addr).await.expect("Can't listen");
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(handler(stream));
+    }
 }
