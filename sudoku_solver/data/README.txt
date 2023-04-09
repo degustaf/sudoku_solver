a3bb4e76708308d9388e3625634137ad0f0c7c1c
Derek Gustafson <degustaf@gmail.com>
2023-04-09 15:31:09 -0400

    Add ability to solve multiple puzzles in one file.

---
+++ sudoku_solver/data/README.txt
@@ -0,0 +1,2 @@
+The data in this directory is copied from https://github.com/norvig/pytudes.
+It is licensed under the MIT license, included in this directory.
--- sudoku_solver/src/main.rs
+++ sudoku_solver/src/main.rs
@@ -1,25 +1,58 @@
 //! A command line utility for solving sudoku.
 
 use clap::{Parser, Subcommand};
-use sudoku_engine;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::{Path, PathBuf};
 
 #[derive(Subcommand)]
 enum Command {
     /// Take a string representation of a grid from the command line and solve it.
     Solve { repr: String },
+
+    /// Treat each line of a file as an individual puzzle, and solve all of them.
+    FromFile { path: PathBuf },
 }
 
-fn solve_puzzle(repr: String) {
-    fn helper(repr: String) -> Result<sudoku_engine::Board, sudoku_engine::SudokuErrors> {
-        let board = sudoku_engine::from_string(&repr)?;
-        sudoku_engine::solve(&board)
-    }
-    match helper(repr) {
+fn solve_helper(repr: &str) -> Result<sudoku_engine::Board, sudoku_engine::SudokuErrors> {
+    let board = sudoku_engine::from_string(&repr)?;
+    sudoku_engine::solve(&board)
+}
+
+fn solve_puzzle(repr: &str) {
+    match solve_helper(repr) {
         Ok(_board) => {
             println!("Solved!");
         }
         Err(e) => {
             eprintln!("Error: {e}");
+        }
+    }
+}
+
+fn solve_file(path: &Path) {
+    let file = match File::open(path) {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("Error: {e}");
+            return;
+        }
+    };
+    let data = BufReader::new(file);
+
+    for (i, line) in data.lines().enumerate() {
+        let repr = match line {
+            Ok(repr) => repr,
+            Err(e) => {
+                eprintln!("Error on line {i}: {e}");
+                return;
+            }
+        };
+        match solve_helper(&repr) {
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("Error on line {i}: {e}");
+            }
         }
     }
 }
@@ -35,6 +68,7 @@ fn main() {
     let args = Args::parse();
 
     match args.cmd {
-        Command::Solve { repr } => solve_puzzle(repr),
+        Command::Solve { repr } => solve_puzzle(&repr),
+        Command::FromFile { path } => solve_file(&path),
     }
 }
